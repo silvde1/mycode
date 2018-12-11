0b443a5548fbde66f51f9697da046a4e5d23f05d
silvde1 <dennis.silva2@verizonwireless.com>
2018-12-11 09:29:35 -0500

    reason for commit

---
+++ Dennis/lab_input/input_ip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+########Written By: Dennis Silva
+########Collect IP address information from a user, and display it back to them.
+
+#########Collect input from the user
+user_input = input('Please enter an IPv4 IP Address:')
+
+#########Print out the input collected from the user
+print("you told me the IPv4 address is:" + user_input)
+
+##########Collect vendor name from user
+vendor_name = input('Please enter the vendor name')
+
+##########Print out the vendor name
+print("you told me the vendor name is:" + vendor_name)
